b5274c2fadee06c83cce90022644fb70870d0fbf
Adam Selker <adamselker@gmail.com>
2018-10-07 22:55:58 -0400

    Begin work on shiftregister test bench.
    Write a function to check equivalence / print if not, and change names.  Does it compile? maybe!

--- shiftregister.t.v
+++ shiftregister.t.v
@@ -5,24 +5,42 @@
 module testshiftregister();
 
     reg             clk;
-    reg             peripheralClkEdge;
-    reg             parallelLoad;
-    wire[7:0]       parallelDataOut;
-    wire            serialDataOut;
-    reg[7:0]        parallelDataIn;
-    reg             serialDataIn; 
+    reg             pclk;
+    reg             pload;
+    wire[7:0]       pdataOut;
+    wire            sdataOut;
+    reg[7:0]        pdataIn;
+    reg             sdataIn; 
     
     // Instantiate with parameter width = 8
     shiftregister #(8) dut(.clk(clk), 
-    		           .peripheralClkEdge(peripheralClkEdge),
-    		           .parallelLoad(parallelLoad), 
-    		           .parallelDataIn(parallelDataIn), 
-    		           .serialDataIn(serialDataIn), 
-    		           .parallelDataOut(parallelDataOut), 
-    		           .serialDataOut(serialDataOut));
+    		           .pclk(pclk),
+    		           .pload(pload), 
+    		           .pdataIn(pdataIn), 
+    		           .sdataIn(sdataIn), 
+    		           .pdataOut(pdataOut), 
+    		           .sdataOut(sdataOut));
     
+		function check;
+			input name, is, shouldBe;
+			begin
+				if is != shouldBe
+					$display("%s: actually %d, should be %d", name, is, shouldBe);
+			end
+		endfunction
+
     initial begin
-    	// Your Test Code
+			// Clear the shift register so it's in a known state
+			clk = 0; pclk = 0;#100
+			pdataIn = 8b'00000000;
+			pload = 1;
+			clk = 1; #100
+			clk = 0; #100
+			// Fron now on, "leave" the clock at 0, so if behaviors don't happen at the rising edge it's visible
+
+			// Test serial in / parallel out
+
+
     end
 
 endmodule
--- shiftregister.v
+++ shiftregister.v
@@ -10,12 +10,12 @@ module shiftregister
 #(parameter width = 8)
 (
 input               clk,                // FPGA Clock
-input               peripheralClkEdge,  // Edge indicator
-input               parallelLoad,       // 1 = Load shift reg with parallelDataIn
-input  [width-1:0]  parallelDataIn,     // Load shift reg in parallel
-input               serialDataIn,       // Load shift reg serially
-output [width-1:0]  parallelDataOut,    // Shift reg data contents
-output              serialDataOut       // Positive edge synchronized
+input               pclk,  // Edge indicator
+input               pload,       // 1 = Load shift reg with parallelDataIn
+input  [width-1:0]  pdataIn,     // Load shift reg in parallel
+input               sdataIn,       // Load shift reg serially
+output [width-1:0]  pdataOut,    // Shift reg data contents
+output              sdataOut       // Positive edge synchronized
 );
 
     reg [width-1:0]      shiftregistermem;
