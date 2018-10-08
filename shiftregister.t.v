953b05f04950a5a01cf8fa71fc7dfa0ca2820823
Adam Selker <adamselker@gmail.com>
2018-10-08 11:10:09 -0400

    Un-factored check function, because Verilog sucks

--- shiftregister.t.v
+++ shiftregister.t.v
@@ -1,6 +1,7 @@
 //------------------------------------------------------------------------
 // Shift Register test bench
 //------------------------------------------------------------------------
+`include "shiftregister.v"
 
 module testshiftregister();
 
@@ -21,27 +22,27 @@ module testshiftregister();
     		           .pdataOut(pdataOut), 
     		           .sdataOut(sdataOut));
     
-		function check;
-			input name, is, shouldBe;
-			begin
-				if is != shouldBe
-					$display("%s: actually %d, should be %d", name, is, shouldBe);
-			end
-		endfunction
-
     initial begin
 			// Clear the shift register so it's in a known state
 			clk = 0; pclk = 0;#100
-			pdataIn = 8b'00000000;
+			pdataIn = 8'b00000000;
 			pload = 1;
-			clk = 1; #100
-			clk = 0; #100
+			clk = 1; #100;
+			clk = 0; #100;
 			// Fron now on, "leave" the clock at 0, so if behaviors don't happen at the rising edge it's visible
 
 			// Test serial in / parallel out
-
-
-    end
+			sdataIn = 1; pclk = 1; #100
+			clk = 1; #100; clk = 0; #100;
+			clk = 1; #100; clk = 0; #100;
+			clk = 1; #100; clk = 0; #100;
+			clk = 1; #100; clk = 0; #100;
+			// CHECK "Serial in / parallel out", pdataOut, 8'b01001111
+			if (pdataOut !== 8'b01001111)
+				$display("%s: actually %b, should be %b", "Serial in / parallel out", pdataOut, 8'b01001111);
+			#100;
+		
+		end
 
 endmodule
 
