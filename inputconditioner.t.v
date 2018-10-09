ca0dbb7bf150a85f48090a011677dfaa2637aaae
nathanyee <nathaniel.yee@students.olin.edu>
2018-10-09 16:46:10 -0400

    made test cases

--- inputconditioner.t.v
+++ inputconditioner.t.v
@@ -29,13 +29,53 @@ module testConditioner();
     initial begin
 			$dumpfile("inputconditioner.vcd");
 			$dumpvars(0, dut);
-			$monitor("t=%d, pin=%d, conditioned=%d, rising=%d, falling=%d", $time, pin, conditioned, rising, falling);
+			// $monitor("t=%d, pin=%d, conditioned=%d, rising=%d, falling=%d", $time, pin, conditioned, rising, falling);
 			
 			pin = 0; #100
-			pin = 1; #200
-			pin = 0; #200
-            pin = 1; #80
-			pin = 0; #100
+
+            // Synchronization & Edge Detection test
+			pin = 1; #85 // stop pin 5 units of time before lower half clock cycle finishes
+            pin = 0; #5 // turn pin off and wait 5 units of time for clock cycle to finish
+            pin = 0; #20 // wait a complete clock cycle
+            $display("t=%d, conditioned=%d, risingEdge=%d", $time, conditioned, rising);
+            pin = 0; #1 // conditioned should turn after with rising edge 
+            $display("t=%d, conditioned=%d, risingEdge=%d", $time, conditioned, rising);
+			pin = 0; #79 // conditioned should be on right before falling edge
+            $display("t=%d, conditioned=%d, fallingEdge=%d", $time, conditioned, falling);
+			pin = 0; #1 // conditioned shoudl turn off with falling edge
+            $display("t=%d, conditioned=%d, fallingEdge=%d", $time, conditioned, falling);
+
+            // Debouncing
+            pin = 0; #100 // let signal reset
+
+            pin = 1; #10 // input bouncy signal
+            pin = 0; #5
+            pin = 1; #13
+            pin = 0; #15
+            pin = 1; #7
+            pin = 0; #9
+            pin = 1; #8
+            pin = 0; #4
+            pin = 1; #12
+            pin = 0; #19
+            pin = 1; #10
+            pin = 0; #11
+            pin = 1; #6
+            pin = 0; #14
+            pin = 1; #8
+            pin = 0; #9
+            pin = 1; #2
+            pin = 0; #3
+            pin = 1; #4
+            pin = 0; #5
+            pin = 1; #17
+            pin = 0; #16
+            pin = 1; #10
+            pin = 0; #10
+            pin = 1; #10
+            pin = 0; #10
+            pin = 1; #10
+            pin = 0; #10
 
 
 			$finish;
