e0197f2f8ec3c3a4ff1704c470517c0df7e74dce
Adam Selker <adamselker@gmail.com>
2018-10-18 19:00:28 -0400

    Rename fsm2 -> finitestatemachine

--- corelogic.v
+++ corelogic.v
@@ -2,7 +2,7 @@
 // Everything except the input conditioners and tri-state buffer
 // (unfortunately, "logic" is a reserved word)
 
-`include "fsm2.v"
+`include "finitestatemachine.v"
 `include "shiftregister.v"
 `include "datamemory.v"
 `include "addresslatch.v"
@@ -17,8 +17,7 @@ module corelogic
 	wire [7:0] dm_out, sr_contents;
 	wire [6:0] dm_addr;
 
-	// finiteStateMachine fsm(sck, cs, mosi, miso_we, dm_we, addr_we, sr_we);
-	fsm2 fsm(clk, sck, cs, mosi, miso_we, dm_we, addr_we, sr_we);
+	finitestatemachine fsm(clk, sck, cs, mosi, miso_we, dm_we, addr_we, sr_we);
 	shiftregister sr(clk, sck, sr_we, dm_out, mosi, sr_contents, miso);
 	addresslatch al(clk, sr_contents[6:0], addr_we, dm_addr);
 	datamemory dm(clk, dm_out, dm_addr, dm_we, sr_contents);
--- finitestatemachine.v
+++ finitestatemachine.v
@@ -1,73 +1,75 @@
-
-
-module finiteStateMachine
+module finitestatemachine
 (
-    input sclk_pin,      // serial clock in
-    input cs_pin,        // chip select 
-    input mosi_pin,
-    output miso_pin,
-    output dm_we,
-    output addr_we,
-    output sr_we
+	input clk,
+	input sck, 
+	input cs,      
+	input mosi,
+	output miso_en,
+	output reg dm_we,
+	output reg addr_we,
+	output reg sr_we
 );
 
-    reg [1:0] state;
-    reg [2:0] number;
 
-    localparam J = 2'0;
-    localparam A = 2'1;
-    localparam R = 2'2;
-    localparam W = 2'3;
+	localparam J = 1; // Wait
+	localparam A = 2; // Address
+	localparam Rw = 3; // Read or write?
+	localparam R = 4; // Read
+	localparam W = 5; // write
 
-    assign miso_pin = 1;
+	reg [2:0] state = J;
+	reg [2:0] substate = 0;
+	reg addr_we_next_cycle = 0;
+	reg sck_was = 0;
 
-    always @(posedge sclk_pin) begin
-        case(state)
-            // J: begin
-            // end
-            A: begin // Address
-                dm_we = 0;
-                sr_we = 0;
+	assign miso_en = 1; // We can just leave it enabled
 
-                case(number)
-                    0: begin // check mosi_pin to determine Read/write
-                        addr_we = 0; // TODO: check timing
-                        if (mosi_pin) state = R;
-                        else state = W;
-                        number = 7;
-                    end
-                    1: begin // write address to address latch
-                        addr_we = 1; // TODO: check timing
-                        number = number - 1;
-                    end
-                    default: begin // let shift register read in address data
-                        addr_we = 0;
-                        number = number - 1;
-                    end
+	always @(posedge clk) begin
 
-            end
-            R: begin // Read
-                case (number)
-                    7: begin
-                        sr_we = 1; // TODO: check timing
-                        number = number - 1;
-                    end
-                    0: begin
-                        sr_we = 0; // TODO: check timing
-                        state = A; number = 7;
-                    default: begin 
-                        sr_we = 0; // TODO: check timing
-                        number = number - 1;
-                    end
-            end
-            W: begin // Write
-                case (number)
-                    0: begin
-                        dm_we = 1;  // TODO: check timing
-                        state = A; number = 7;
-                    default: begin 
-                        number = number - 1;
-                    end
-            end
+		if (state == J && !cs) begin
+			$display("t = %d, go to state A", $time);
+			state = A;
+			substate = 6;
+		end
+			
+		if (sck && !sck_was) begin
+			case(state)
+				A: if (substate == 0) begin
+						$display("t = %d, go to state Rw", $time);
+						addr_we_next_cycle = 1; // Save the address
+						state = Rw;
+					end else substate = substate - 1;
+				Rw: if (mosi == 1) begin
+						$display("t = %d, go to state R", $time);
+						sr_we = 1;
+						state = R; substate = 7;
+					end else begin
+						$display("t = %d, go to state W", $time);
+						state = W; substate = 7;
+					end
+				R: if (substate == 0) begin
+						$display("t = %d, go to state J", $time);
+						state = J; 
+					end else substate = substate - 1;
+				W: if (substate == 0) begin
+						$display("t = %d, go to state J", $time);
+						dm_we = 1;
+						state = J;
+					end else substate = substate - 1;
+			endcase
+		end
+	end
+
+	always @(negedge clk) begin
+		if (addr_we_next_cycle == 1) begin
+			addr_we = 1;
+			addr_we_next_cycle = 0;
+		end else 	addr_we = 0;
+
+		sr_we = 0;
+		dm_we = 0;
+		sck_was = sck;
+	end
+
 
 endmodule
