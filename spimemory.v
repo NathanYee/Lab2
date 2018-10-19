41156a061376465ff18437f4743c3e2d68bd0ee3
nathanyee <nathaniel.yee@students.olin.edu>
2018-10-18 20:52:31 -0400

    added fsm and sr to spimemory

--- spimemory.v
+++ spimemory.v
@@ -18,10 +18,21 @@ module spiMemory
     output [3:0]    leds        // LEDs for debugging
 )
 
+    // Condition inputs
     wire mosi_cond, pos_edge, neg_edge, cs_cond;
     inputconditioner mosi_c(clk, mosi_pin, mosi_cond, _, _);
     inputconditioner sclk_c(clk, sclk_pin, _, pos_edge, neg_edge);
     inputconditioner cs_c(clk, cs_pin, cs_cond, _, _);
 
+    // Finite State Machine
+    wire miso_we, dm_we, sr_we, addr_we;
+    finitestatemachine fsm(clk, pos_edge, cs_cond, mosi_cond, miso_we, dm_we, addr_we, sr_we);
+
+    // Shift Register
+   	wire [7:0] dm_out, sr_contents;
+    shiftregister sr(clk, pos_edge, sr_we, dm_out, mosi_cond, sr_contents);
+
+
+	wire [6:0] dm_addr;
 endmodule
    
